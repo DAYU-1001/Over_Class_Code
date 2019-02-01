69df105812703866b61ce608cc70279f3bfef76c
RuYunW <42913632+RuYunW@users.noreply.github.com>
2019-02-01 18:05:23 +0800

    add Rastrigin.m

---
+++ 萝卜代码开会/经典测试函数2_Rastrigin函数/Rastrigin_code.m
@@ -0,0 +1,17 @@
+%Griewank 函数
+clc;
+clear;
+
+%绘制Griewank函数图形
+x=[-5:0.1:5];
+y=x;
+[X,Y]=meshgrid(x,y);
+[row,col]=size(X);
+for l =1:col
+    for h=1:row
+        z(h,l)=Rastrigin([X(h,l),Y(h,l)]);
+    end
+end
+mesh(X,Y,z);
+%view([-15.5 30]);
+shading interp
