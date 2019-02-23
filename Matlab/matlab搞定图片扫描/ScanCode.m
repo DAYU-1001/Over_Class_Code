755b47e47d1d2537420ff516170417d5d43f3cad
RuYunW <42913632+RuYunW@users.noreply.github.com>
2019-02-23 21:49:01 +0800

    update scan code

--- Matlab/matlab搞定图片扫描/ScanCode.m
+++ Matlab/matlab搞定图片扫描/ScanCode.m
@@ -1,38 +1,24 @@
 clc;
 clear all;
-filename='TestPic.jpg';%图片名称，默认当前文件夹
-img=imread(filename);%图片读入
+filename='TestPic.jpg';%图片名称，默认与代码在同一文件夹下
+img=imread(filename);%读入图片
 R=img(:,:,1);
 G=img(:,:,2);
 B=img(:,:,3);
-
-[x,y,z]=size(img);
-
-for i=1:x
-    for j=1:y
-        if ((R(i,j)>=140)&&(R(i,j)<=255)&&(G(i,j)<100)&&(B(i,j)<100))%如果是红色（图章）
-            R(i,j)=255;
-            G(i,j)=70;
-            B(i,j)=70;
-        else
-            %Gray = R*0.299 + G*0.587 + B*0.114    灰度与RGB转换公式
-            gray=R(i,j)*0.299+G(i,j)*0.587+B(i,j)*0.114;
-            if gray>120  %120为一灰度阈值，若大于这一阈值，全部设为白色，120为一经验值
-                R(i,j)=255;
-                G(i,j)=255;
-                B(i,j)=255;
-            end
-        end
-    end
-end
+red=(R>=100).*(R<=255).*(G<110).*(B<110);
+R(red==1)=255;
+G(red==1)=70;
+B(red==1)=70;
+gray=R.*0.299+G.*0.587+B.*0.114;
+white=(gray>120)-red;
+R(white==1)=255;
+G(white==1)=255;
+B(white==1)=255;
 disp('转换结束');
-for i=1:x
-    disp(['颜色覆盖',num2str(i/x*100),'%']);%显示覆盖进度
-    for j=1:y
-        res(i,j,1) = R(i,j);
-        res(i,j,2) = G(i,j);
-        res(i,j,3) = B(i,j);
-    end
-end
+res(:,:,1)=R(:,:);
+res(:,:,2)=G(:,:);
+res(:,:,3)=B(:,:);
 imwrite(res,'stripes2.png');%保存图片
 disp('图片已保存');
+
+
